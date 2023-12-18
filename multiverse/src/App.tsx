46b2222e1cff6e22f599200edf70a43f96898603
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-12-19 08:24:46 +1100

    add dojo client

--- multiverse/src/App.tsx
+++ multiverse/src/App.tsx
@@ -3,69 +3,74 @@ import "./App.css";
 import { client } from "./server";
 
 function App() {
-  const [image, setImage] = useState("");
-  const [portrait, setPortrait] = useState("");
-  // const [music, setMusic] = useState(
-  //   "https://replicate.delivery/pbxt/eCSi52ISai1YVyN6jqrNjfNuAMhBEHzGaPPFpYOKUg3UbDDSA/out.wav"
-  // );
-  const [isPlaying, setIsPlaying] = useState(false);
+    const [image, setImage] = useState("");
+    const [portrait, setPortrait] = useState("");
+    const [music, setMusic] = useState(
+        "https://replicate.delivery/pbxt/eCSi52ISai1YVyN6jqrNjfNuAMhBEHzGaPPFpYOKUg3UbDDSA/out.wav"
+    );
+    const [isPlaying, setIsPlaying] = useState(false);
 
-  const audioRef = useRef<HTMLAudioElement | null>(null);
+    const audioRef = useRef<HTMLAudioElement | null>(null);
 
-  useEffect(() => {
-    const input = async () => {
-      const background = await client.world.image.query({
-        name: "anthropomorphic highly detailed group portrait of cute mr bean grey goblins eating trading card game, intricate, elegant, highly detailed grey goblin, digital painting, artstation, concept art, smooth, sharp focus, illustration, art by artgerm, bob eggleton, michael whelan, stephen hickman, richard corben, wayne barlowe, trending on artstation and greg rutkowski and alphonse mucha, 8 k",
-      });
+    useEffect(() => {
+        const input = async () => {
+            const background = await client.world.image.query({
+                name: "single realistic balrog",
+            });
 
-      const portrait = await client.world.image.query({
-        name: "the eldritch knight as a realistic fantasy knight, closeup portrait art by donato giancola and greg rutkowski, digital art, trending on artstation, symmetry! !",
-      });
+            const portrait = await client.world.image.query({
+                name: "the eldritch knight as a realistic fantasy knight, closeup portrait art by donato giancola and greg rutkowski, digital art, trending on artstation, symmetry! !",
+            });
 
-      // const music = await client.hello.music.query({
-      //   name: "dark melancholy vibes, pads and synths, deep tribal drum, slow paced ",
-      // });
+            // const music = await client.hello.music.query({
+            //   name: "dark melancholy vibes, pads and synths, deep tribal drum, slow paced ",
+            // });
 
-      // console.log(music);
+            // console.log(music);
 
-      // setMusic(music);
+            // setMusic(music);
 
-      setImage(background[0]);
-      setPortrait(portrait[0]);
-    };
+            setImage(background[0]);
+            setPortrait(portrait[0]);
+        };
 
-    input();
-    // audioRef.current?.play();
-  }, []);
+        input();
+        // audioRef.current?.play();
+    }, []);
 
-  // useEffect(() => {
-  //   audioRef.current?.play();
-  // }, [music]);
+    // useEffect(() => {
+    //     audioRef.current?.play();
+    // }, [music]);
 
-  return (
-    <>
-      {/* <audio
-        ref={audioRef}
-        preload="metadata"
-        onPlaying={() => setIsPlaying(true)}
-        onPause={() => setIsPlaying(false)}
-        loop
-      >
-        <source type="audio/mpeg" src={music} />
-      </audio> */}
-      {!image ? (
-        <h1 className="text-4xl text-center">Loading...</h1>
-      ) : (
-        <div className="relative">
-          <img className="w-screen h-screen object-cover" src={image} alt="" />
+    return (
+        <>
+            <audio
+                ref={audioRef}
+                preload="metadata"
+                onPlaying={() => setIsPlaying(true)}
+                onPause={() => setIsPlaying(false)}
+                loop
+            >
+                <source type="audio/mpeg" src={music} />
+            </audio>
+            {!image ? (
+                <h1 className="text-4xl text-center">Loading...</h1>
+            ) : (
+                <div className="relative">
+                    <img
+                        className="w-screen h-screen object-cover"
+                        src={image}
+                        alt=""
+                    />
 
-          <div className="w-full bg-black h-48 absolute bottom-0 left-0 flex justify-center">
-            <img className="w-48 h-48 " src={portrait} alt="" />
-          </div>
-        </div>
-      )}
-    </>
-  );
+                    <div className="w-full bg-black h-48 absolute bottom-0 left-0 flex justify-center">
+                        <img className="w-48 h-48 " src={portrait} alt="" />
+                    </div>
+                    <div className="absolute top-0 bg-white z-10 right-0 h-screen"></div>
+                </div>
+            )}
+        </>
+    );
 }
 
 export default App;
--- server/src/router.ts
+++ server/src/router.ts
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const replicate = new Replicate({
-  auth: process.env.REPLICATE_AUTH,
+    auth: process.env.REPLICATE_AUTH,
 });
 
 const t = initTRPC.create();
@@ -15,54 +15,54 @@ const publicProcedure = t.procedure;
 const router = t.router;
 
 const worldRouter = router({
-  image: publicProcedure
-    .input(z.object({ name: z.string() }).nullish())
-    .query(({ input }) => {
-      const runModel = async () => {
-        const output = await replicate.run(
-          "fofr/sdxl-turbo:6244ebc4d96ffcc48fa1270d22a1f014addf79c41732fe205fb1ff638c409267",
-          {
-            input: {
-              prompt: input?.name,
-              width: 1000,
-              height: 1000,
-              num_outputs: 1,
-              num_inference_steps: 4,
-              agree_to_research_only: true,
-            },
-          }
-        );
+    image: publicProcedure
+        .input(z.object({ name: z.string() }).nullish())
+        .query(({ input }) => {
+            const runModel = async () => {
+                const output = await replicate.run(
+                    "fofr/sdxl-turbo:6244ebc4d96ffcc48fa1270d22a1f014addf79c41732fe205fb1ff638c409267",
+                    {
+                        input: {
+                            prompt: input?.name,
+                            width: 512,
+                            height: 512,
+                            num_outputs: 1,
+                            num_inference_steps: 3,
+                            agree_to_research_only: true,
+                        },
+                    }
+                );
 
-        return output;
-      };
-      return runModel();
-    }),
+                return output;
+            };
+            return runModel();
+        }),
 
-  music: publicProcedure
-    .input(z.object({ name: z.string() }).nullish())
-    .query(({ input }) => {
-      const runModel = async () => {
-        const output = await replicate.run(
-          "meta/musicgen:b05b1dff1d8c6dc63d14b0cdb42135378dcb87f6373b0d3d341ede46e59e2b38",
-          {
-            input: {
-              model_version: "stereo-melody-large",
-              prompt: input?.name,
-              duration: 30,
-            },
-          }
-        );
+    music: publicProcedure
+        .input(z.object({ name: z.string() }).nullish())
+        .query(({ input }) => {
+            const runModel = async () => {
+                const output = await replicate.run(
+                    "meta/musicgen:b05b1dff1d8c6dc63d14b0cdb42135378dcb87f6373b0d3d341ede46e59e2b38",
+                    {
+                        input: {
+                            model_version: "stereo-melody-large",
+                            prompt: input?.name,
+                            duration: 30,
+                        },
+                    }
+                );
 
-        console.log(output);
+                console.log(output);
 
-        return output;
-      };
-      return runModel();
-    }),
+                return output;
+            };
+            return runModel();
+        }),
 });
 
 export const appRouter = router({
-  world: worldRouter,
+    world: worldRouter,
 });
 
 export type AppRouter = typeof appRouter;
