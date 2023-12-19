40ffd2b33649cdc3cfcf7ed54ffdb36f583d132f
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-12-19 12:47:16 +1100

    typed

--- multiverse/src/dojo/generated/Slayer.ts
+++ multiverse/src/dojo/generated/Slayer.ts
@@ -9,6 +9,22 @@ export interface CreateProps extends Signer {
     name: string;
 }
 
+export interface SeekProps extends Signer {
+    name: string;
+}
+
+export interface RollProps extends Signer {
+    orders: string;
+}
+
+export interface BuyProps extends Signer {
+    item: string;
+}
+
+export interface ApplyProps extends Signer {
+    item: string;
+}
+
 export class Slayer extends RPCProvider {
     public play: PlayContract;
 
@@ -21,6 +37,10 @@ export class Slayer extends RPCProvider {
 
 interface IPlayContractFunctions {
     create: (props: CreateProps) => Promise<any>;
+    seek: (props: Signer) => Promise<any>;
+    roll: (props: RollProps) => Promise<any>;
+    buy: (props: BuyProps) => Promise<any>;
+    apply: (props: ApplyProps) => Promise<any>;
 }
 
 export class PlayContract implements IPlayContractFunctions {
@@ -42,6 +62,56 @@ export class PlayContract implements IPlayContractFunctions {
             );
         } catch (error) {
             console.error("Error in creating entity:", error);
+            throw error;
+        }
+    }
+
+    async seek(props: Signer): Promise<any> {
+        try {
+            return await this.slayer.execute(
+                props.account,
+                this.name,
+                "seek",
+                []
+            );
+        } catch (error) {
+            console.error("Error in seeking entity:", error);
+            throw error;
+        }
+    }
+
+    async roll(props: RollProps): Promise<any> {
+        try {
+            return await this.slayer.execute(props.account, this.name, "roll", [
+                props.orders,
+            ]);
+        } catch (error) {
+            console.error("Error in rolling entity:", error);
+            throw error;
+        }
+    }
+
+    async buy(props: BuyProps): Promise<any> {
+        try {
+            return await this.slayer.execute(props.account, this.name, "buy", [
+                props.item,
+            ]);
+        } catch (error) {
+            console.error("Error in buying entity:", error);
+            throw error;
+        }
+    }
+
+    async apply(props: ApplyProps): Promise<any> {
+        try {
+            return await this.slayer.execute(
+                props.account,
+                this.name,
+                "apply",
+                [props.item]
+            );
+        } catch (error) {
+            console.error("Error in applying entity:", error);
             throw error;
         }
     }
