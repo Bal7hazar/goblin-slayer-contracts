e6dd6a7b4452b9c87d264e51a4889361237ac086
bal7hazar <bal7hazar@proton.me>
2023-12-30 16:15:28 +0100

    🐛 Fix missing file

--- client/src/dojo/setupNetwork.ts
+++ client/src/dojo/setupNetwork.ts
@@ -1,11 +1,26 @@
 import { defineContractComponents } from "./contractComponents";
 import { world } from "./world";
 import { Account, num } from "starknet";
-import manifest from "./manifest.json";
 import * as torii from "@dojoengine/torii-client";
 import { Slayer } from "./generated/Slayer";
 
 export type SetupNetworkResult = Awaited<ReturnType<typeof setupNetwork>>;
+
+async function loadManifest() {
+    try {
+        if (import.meta.env.VITE_PUBLIC_DEV) {
+            const devManifest = await import(
+                "../../../contracts/target/dev/manifest.json"
+            );
+            return devManifest;
+        }
+    } catch (error) {
+        console.warn(
+            "Development manifest not found, using production manifest."
+        );
+    }
+    return import("./manifest.json");
+}
 
 export async function setupNetwork() {
     // Extract environment variables for better readability.
@@ -13,15 +28,13 @@ export async function setupNetwork() {
         VITE_PUBLIC_WORLD_ADDRESS,
         VITE_PUBLIC_NODE_URL,
         VITE_PUBLIC_TORII,
-        VITE_PUBLIC_DEV,
     } = import.meta.env;
 
     // Create a new RPCProvider instance.
+    const manifest = await loadManifest();
     const provider = new Slayer(
         VITE_PUBLIC_WORLD_ADDRESS,
-        VITE_PUBLIC_DEV
-            ? await import("../../../contracts/target/dev/manifest.json")
-            : manifest,
+        manifest,
         VITE_PUBLIC_NODE_URL
     );
 
